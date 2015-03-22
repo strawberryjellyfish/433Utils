07996c7091e68af59a01544c3ef5c110962a199c
Robert Miller <rob@strawberryjellyfish.com>
2015-03-22 16:36:24 +1030

    Adding pulse length, protocol, bit length to RFSniffer output

--- RPi_utils/RFSniffer.cpp
+++ RPi_utils/RFSniffer.cpp
@@ -9,48 +9,45 @@
 #include "RCSwitch.h"
 #include <stdlib.h>
 #include <stdio.h>
-     
-     
+
+
 RCSwitch mySwitch;
- 
 
 
 int main(int argc, char *argv[]) {
-  
+
      // This pin is not the first pin on the RPi GPIO header!
      // Consult https://projects.drogon.net/raspberry-pi/wiringpi/pins/
      // for more information.
      int PIN = 2;
-     
+
      if(wiringPiSetup() == -1)
        return 0;
 
      mySwitch = RCSwitch();
      mySwitch.enableReceive(PIN);  // Receiver on inerrupt 0 => that is pin #2
-     
-    
+
      while(1) {
-  
+
       if (mySwitch.available()) {
-    
+
         int value = mySwitch.getReceivedValue();
-    
+
         if (value == 0) {
-          printf("Unknown encoding");
-        } else {    
-   
-          printf("Received %i\n", mySwitch.getReceivedValue() );
+            printf("Unknown encoding");
+        } else {
+            printf("Received %i\n", mySwitch.getReceivedValue() );
+            printf("Pulse Length %i\n", mySwitch.getReceivedDelay() );
+            printf("Bit Length %i\n", mySwitch.getReceivedBitlength() );
+            printf("Protocol %i\n", mySwitch.getReceivedProtocol() );
         }
-    
+
         mySwitch.resetAvailable();
-    
+
       }
-      
-  
+
   }
 
   exit(0);
 
-
 }
-
